f3abaab9788768cd8b73560b74a307c53c007db6
kaisei0326 <kaisei0326@gmail.com>
2019-10-07 08:30:01 +0900

    【add】おもしれーコード...

--- app/src/main/java/com/example/gettext/MainActivity.kt
+++ app/src/main/java/com/example/gettext/MainActivity.kt
@@ -2,40 +2,66 @@ package com.example.gettext
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.FuelError
+import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.core.extensions.cUrlString
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
+import com.github.kittinunf.fuel.coroutines.awaitStringResponseResult
+import kotlinx.coroutines.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = MainActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getButton.setOnClickListener {
-            var data = ""
+        FuelManager.instance.apply {
+            basePath = "http://httpbin.org"
+            baseHeaders = mapOf("Device" to "Android")
+            baseParams = listOf("key" to "value")
+        }
 
-            val httpAsync = "http://192.168.43.230:8080/ranking/search?name=taro"
-                .httpGet()
-                .responseString { _, _, result ->
-                    when (result) {
-                        is Result.Failure -> println(result.getException())
-                        is Result.Success -> data = result.get()
-                    }
-                }
-            httpAsync.join()
+        GET.setOnClickListener {
+            GlobalScope.launch(Dispatchers.Main, CoroutineStart.DEFAULT) {
+                httpGet()
+            }
+        }
 
-            text_view.text = data
+        POST.setOnClickListener {
+            GlobalScope.launch(Dispatchers.Main, CoroutineStart.DEFAULT) {
+                httpPost()
+            }
+        }
+
+        mainClearButton.setOnClickListener {
+            mainResultText.text = ""
+            mainAuxText.text = ""
         }
     }
+
+    private suspend fun httpGet() {
+        val (request, response, result) = Fuel.get("/get", listOf("userId" to "123")).awaitStringResponseResult()
+        Log.d(TAG, response.toString())
+        Log.d(TAG, request.toString())
+        update(result)
+    }
+
+    private suspend fun httpPost() {
+        val (request, response, result) = Fuel.post("/post", listOf("userId" to "123")).awaitStringResponseResult()
+        Log.d(TAG, response.toString())
+        Log.d(TAG, request.toString())
+        update(result)
+    }
+
+    private fun <T : Any> update(result: Result<T, FuelError>) {
+        result.fold(success = {
+            mainResultText.append(it.toString())
+        }, failure = {
+            mainResultText.append(String(it.errorData))
+        })
+    }
 }
-
-/*
-    val message = getString(R.string.message, "taro")
-    val textView = findViewById<TextView>(R.id.text_view)
-    textView.setText(message)
-
-    val accessor = HttpAccessor()
-    val resultJSON = accessor.getJson("http://localhost:8080/ranking/search?name=taro")
-    val name = resultJSON.getString("name")
-    text_view.text = resultJSON.string("name")
-*/
